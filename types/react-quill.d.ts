b2ffc15e5e64e89a499ec11d9d567b3447ecd093
Kuifang.Liu <jiangnan@jiangnan24.ml>
2025-02-04 21:26:26 +0800

    修复编译报错

--- types/react-quill.d.ts
+++ types/react-quill.d.ts
@@ -1,4 +1,16 @@
 declare module 'react-quill' {
-  import { ReactQuill } from 'react-quill'
+  import React from 'react'
+  
+  export interface ReactQuillProps {
+    theme?: string
+    value?: string
+    onChange?: (value: string) => void
+    modules?: any
+    formats?: string[]
+    className?: string
+    style?: React.CSSProperties
+  }
+
+  const ReactQuill: React.FC<ReactQuillProps>
   export default ReactQuill
 } 
